816036d8eeb8661c1a81f26aa2c125227935a13d
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2023-08-02 00:25:36 +0300

    added a card to questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import MainCard from "./components/Main/MainCard.jsx";
 import SideCard from "./components/Side/SideCard";
+
+import Divider from "@mui/material/Divider";
+
 function App() {
   return (
     <>
       <Header />
       <main>
         <MainCard />
+        <Divider orientation="vertical" flexItem />
+
         <SideCard />
       </main>
       <Footer />
--- src/components/Main/Questions.jsx
+++ src/components/Main/Questions.jsx
@@ -9,13 +9,18 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
 import classes from "./Questions.module.css";
+import { Divider } from "@mui/material";
+
 const Questions = () => {
   const [activeStep, setActiveStep] = useState(0);
   const theme = useTheme();
@@ -31,86 +36,86 @@ const Questions = () => {
   };
 
   return (
-    <Box className={classes.questionsBox} sx={{ width: "100%", flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          height: 50,
-          pl: 2,
-          bgcolor: "background.default",
-        }}
-      >
-        <Typography sx={{ height: "100%" }}>
-          {questions[activeStep].question}
-        </Typography>
-      </Paper>
-      <Box sx={{ minHeight: 170, width: "100%", p: 2 }}>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          name="radio-buttons-group"
+    <Card className={classes.questionsBox} sx={{ width: "100%", flexGrow: 1 }}>
+      <CardContent>
+        <Box sx={{ display: "flex", textAlign: "left" }}>
+          <Typography component="h3" sx={{ height: "100%" }}>
+            {questions[activeStep].question}
+          </Typography>
+        </Box>
+        <Divider variant="middle" sx={{ m: 4 }} />
+        <Box
+          className={classes.questionsBox}
+          sx={{ minHeight: 170, width: "100%" }}
         >
-          {questions[activeStep].possibleAnswers.map((el, i) =>
-            questions[activeStep].possibleAnswers.length > 1 ? (
-              <FormControlLabel
-                key={i}
-                value={el.answer}
-                control={<Radio />}
-                label={el.answer}
-              />
-            ) : (
-              <TextField
-                key={i}
-                id="filled-basic"
-                label="Place your answer here"
-                variant="filled"
-                fullWidth
-              />
-            )
-          )}
-        </RadioGroup>
-      </Box>
-      <MobileStepper
-        className={classes.stepperButtons}
-        variant="text"
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
           >
-            Next
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
+            {questions[activeStep].possibleAnswers.map((el, i) =>
+              questions[activeStep].possibleAnswers.length > 1 ? (
+                <FormControlLabel
+                  key={i}
+                  value={el.answer}
+                  control={<Radio />}
+                  label={el.answer}
+                />
+              ) : (
+                <TextField
+                  key={i}
+                  id="filled-basic"
+                  label="Place your answer here"
+                  variant="filled"
+                  fullWidth
+                />
+              )
             )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-      <Button
-        className={classes.submitExamButton}
-        variant="contained"
-        position="static"
-      >
-        Submit Exams Answers
-      </Button>
-    </Box>
+          </RadioGroup>
+        </Box>
+        <MobileStepper
+          className={classes.stepperButtons}
+          variant="text"
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
+              Next
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
+        />
+        <Button
+          className={classes.submitExamButton}
+          variant="contained"
+          position="static"
+        >
+          Submit Exam's Answers
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
