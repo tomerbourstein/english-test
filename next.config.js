1280e1c3195e25cf21f8f804328b4e6434d99c47
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2024-01-24 18:17:24 +0200

    changed nextjs config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
